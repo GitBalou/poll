a0f41a7236ce0f1db3261d7009cf020d5bc0b5b3
snipzer <l.arif@laposte.net>
2017-05-30 14:06:42 +0200

    Correction du test de serviceCtrl

--- test/controllers/ServicesCtrl.js
+++ test/controllers/ServicesCtrl.js
@@ -5,11 +5,17 @@ describe("ServicesCtrl", () =>
 {
     describe("#Injection", () =>
     {
-        const SerCtrl = new ServicesCtrl(true);
+        const SerCtrl = new ServicesCtrl(true, true);
 
         it("Le service pollService est injecté en tant que dépendance", (done) =>
         {
             expect(SerCtrl._pollService).toBe(true);
+            done();
+        });
+
+        it("Le service userService est injecté en tant que dépendance", (done) =>
+        {
+            expect(SerCtrl._userService).toBe(true);
             done();
         });
     });
@@ -19,27 +25,29 @@ describe("ServicesCtrl", () =>
         it("Doit retourner un tableau objet contenant tout les sondages si le parametre id de la requête n'est pas présente", (done) =>
         {
             const SerCtrl = new ServicesCtrl({
-                getPolls: () =>
-                {
-                    const ResponseModel = {id: "", label: "", userIdArray: [""]};
+                _pollService: {
+                    getPolls: () =>
+                    {
+                        const ResponseModel = {id: "", label: "", userIdArray: [""]};
 
-                    const poll1 = {
-                        question: "",
-                        userId: "0",
-                        responseArray: [
-                            ResponseModel,
-                        ]
-                    };
+                        const poll1 = {
+                            question: "",
+                            userId: "0",
+                            responseArray: [
+                                ResponseModel,
+                            ]
+                        };
 
-                    const poll2 = {
-                        question: "",
-                        userId: "1",
-                        responseArray: [
-                            ResponseModel,
-                        ]
-                    };
+                        const poll2 = {
+                            question: "",
+                            userId: "1",
+                            responseArray: [
+                                ResponseModel,
+                            ]
+                        };
 
-                    return [poll1, poll2];
+                        return [poll1, poll2];
+                    }
                 }
             });
 
@@ -74,27 +82,29 @@ describe("ServicesCtrl", () =>
         it("Doit retourner un tableau objet contenant les sondages d'un utilisateur si l'id de la requête est présente et valide", (done) =>
         {
             const SerCtrl = new ServicesCtrl({
-                getPoll: (id) =>
-                {
-                    const ResponseModel = {id: "", label: "", userIdArray: [""]};
+                _pollService: {
+                    getPollFromUserId: (id) =>
+                    {
+                        const ResponseModel = {id: "", label: "", userIdArray: [""]};
 
-                    const poll1 = {
-                        question: "",
-                        userId: "0",
-                        responseArray: [
-                            ResponseModel,
-                        ]
-                    };
+                        const poll1 = {
+                            question: "",
+                            userId: "0",
+                            responseArray: [
+                                ResponseModel,
+                            ]
+                        };
 
-                    const poll2 = {
-                        question: "",
-                        userId: "0",
-                        responseArray: [
-                            ResponseModel,
-                        ]
-                    };
+                        const poll2 = {
+                            question: "",
+                            userId: "0",
+                            responseArray: [
+                                ResponseModel,
+                            ]
+                        };
 
-                    return [poll1, poll2];
+                        return [poll1, poll2];
+                    }
                 }
             });
 
@@ -129,7 +139,9 @@ describe("ServicesCtrl", () =>
         it("Doit retourner une erreur 404 si l'id de la requête est présente et non valide", (done) =>
         {
             const SerCtrl = new ServicesCtrl({
-                getPoll: (id) => {return []}
+                _pollService: {
+                    getPollFromUserId: (id) => {return []}
+                }
             });
 
             const req = {
@@ -197,7 +209,9 @@ describe("ServicesCtrl", () =>
         it("Doit renvoyer une erreur 404 si l'id du sondage est présent dans les paramètres de la requête , et, est non présent en base de données", () =>
         {
             const SerCtrl = new ServicesCtrl({
-                getPoll: (id) => { return [] }
+                _pollService: {
+                    getPollFromPollId: (id) => { return [] }
+                }
             });
 
             const body = {
@@ -219,9 +233,11 @@ describe("ServicesCtrl", () =>
                         json: data =>
                         {
                             expect(data).toExist();
+                            expect(data.message).toExist();
+                            expect(data.message).toBeA("sting");
                             expect(data.message).toBe("Error");
                             expect(data.data).toExist();
-                            expect(data.data).toBe("array");
+                            expect(data.data).toBeA("array");
                         }
                     }
                 }
@@ -230,14 +246,96 @@ describe("ServicesCtrl", () =>
             SerCtrl.statsAction(req, res);
         });
 
-        it("Doit renvoyer un objet contenant les statistiques du sondage  si l'id du sondage est présent dans les paramètres de la requête , et, est présent en base de données", () =>
+        it("Doit renvoyer un objet contenant les statistiques du sondage  si l'id du sondage est présent dans les paramètres de la requête , et, est présent en base de données", (done) =>
         {
-            const SerCtrl = new ServicesCtrl();
+            const SerCtrl = new ServicesCtrl({
+                _userService: {
+                    getUserFromUserId: (id) =>
+                    {
+                        let user = {};
+                        switch(id)
+                        {
+                            case 1:
+                                user = {
+                                    _id: 1,
+                                    email: "",
+                                    password: "",
+                                    ville: "Nantes",
+                                    age: "5",
+                                    PollCreated: [],
+                                    PollResponse: [
+                                        {_id: "11", label: "Man", userIdArray: ["1"]},
+                                    ]
+                                };
+                                break;
+                            case 2:
+                                user = {
+                                    _id: 2,
+                                    email: "",
+                                    password: "",
+                                    ville: "Nantes",
+                                    age: "10",
+                                    PollCreated: [],
+                                    PollResponse: [
+                                        {id: "22", label: "Women", userIdArray: ["2"]},
+                                    ]
+                                };
+                                break;
+                            case 3:
+                                user = {
+                                    _id: 3,
+                                    email: "",
+                                    password: "",
+                                    ville: "Paris",
+                                    age: "30",
+                                    PollCreated: [],
+                                    PollResponse: [
+                                        {id: "33", label: "Did you just assume my gender ?", userIdArray: ["3"]},
+                                    ]
+                                };
+                                break;
+                        }
+                        return user;
+                    }
+                },
+                _pollService: {
+                    getPollFromPollId: id =>
+                    {
+                        return poll = {
+                            _id: 0,
+                            question: "Are you man or women ?",
+                            userId: 0,
+                            responseArray: [
+                                {_id: "1", label: "Man", userIdArray: ["1"]},
+                                {_id: "2", label: "Women", userIdArray: ["2"]},
+                                {_id: "3", label: "Did you just assume my gender ?", userIdArray: ["3"]},
+                            ]
+                        };
+                    },
+                    getUserInfoForPoll: id =>
+                    {
+                        const poll = this.getPollFromPollId(id);
+                        let stats = {};
 
-            const body = {
-                message: "Success",
-                data: ["user1", "user2", "user3"]
-            };
+                        poll.responseArray.forEach(response =>
+                        {
+                            stats[response._id] = {
+                                label: response.label,
+                                ages: [],
+                                villes: [],
+                            };
+                            response.userIdArray.forEach(userId =>
+                            {
+                                let user = this._userService.getUserFromId(userId);
+                                stats[response._id].ages.push(user.age);
+                                stats[response._id].villes.push(user.villes);
+                            })
+                        });
+
+                        return stats;
+                    }
+                }
+            });
 
             const req = {
                 params: {
@@ -252,8 +350,25 @@ describe("ServicesCtrl", () =>
                     return {
                         json: data =>
                         {
-                            expect(data.message).toBe(body.message);
-                            expect(data.data.length).toBeGreaterThanOrEqualTo(1);
+                            expect(data).toExist();
+
+                            expect(data.message).toExist();
+                            expect(data.message).toBeA("string");
+                            expect(data.message).toBe("Success");
+
+                            expect(data.data.stats).toExist();
+                            expect(data.data.stats.response1.label).toBeA("string");
+                            expect(data.data.stats.response1.ages).toBeA("array");
+                            expect(data.data.stats.response1.villes).toBeA("array");
+
+                            expect(data.data.stats.response2.label).toBeA("string");
+                            expect(data.data.stats.response2.ages).toBeA("array");
+                            expect(data.data.stats.response2.villes).toBeA("array");
+
+                            expect(data.data.stats.response3.label).toBeA("string");
+                            expect(data.data.stats.response3.ages).toBeA("array");
+                            expect(data.data.stats.response3.villes).toBeA("array");
+                            done();
                         }
                     }
                 }
